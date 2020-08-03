405a24799c193c5fce3afaf28dde19c1895e221c
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2020-08-03 22:27:45 +0530

    🎉

---
+++ README.md
@@ -0,0 +1,28 @@
+# ERDesktop
+#### A screen recording desktop application built using electron and react.
+***
+## Windows Build Instructions:
+
+### Client & Server Setup:
+1. Clone the source repository from Github.
+```
+git clone https://github.com/JackRKelly/ERDesktop.git
+```
+
+2. Install all application dependencies.
+```
+npm install
+```
+
+3. Create production build and run electron.
+```
+npm run build
+npm run electron
+```
+
+***
+### Screenshots:
+
+![ERDesktop](https://i.ibb.co/QD5nCB3/Screenshot-71.png "ERDesktop")
+![Popout Select](https://i.ibb.co/0hS4j9S/Screenshot-72.png "Popout Select")
+![Screen Preview](https://i.ibb.co/dK3Yb6P/Screenshot-69.png "Screen Preview")
---
+++ src/App.js
@@ -0,0 +1,144 @@
+import React, { useState, useRef } from "react";
+import "./App.scss";
+
+function App() {
+  const [selectContent, setSelectContent] = useState("Select Source");
+  const [isRecording, setIsRecording] = useState(false);
+  const videoPreview = useRef(null);
+  const audioPreview = useRef(null);
+  const { desktopCapturer, remote } = window.require("electron");
+  const { Menu } = remote;
+  const { writeFile } = window.require("fs");
+  const { dialog } = remote;
+  let [mediaRecorder, setMediaRecorder] = useState(null);
+  const recordedChunks = [];
+
+  const startVideo = () => {
+    if (mediaRecorder == null) {
+      alert("Select a source to record.");
+    } else if (mediaRecorder.state === "recording") {
+      alert("You must stop recording first.");
+    } else {
+      mediaRecorder.start();
+      setIsRecording(true);
+    }
+  };
+
+  const stopVideo = () => {
+    if (mediaRecorder == null) {
+      alert("Select a source and start recording first.");
+    } else if (mediaRecorder.state === "inactive") {
+      alert("You must start recording first.");
+    } else {
+      mediaRecorder.stop();
+      setIsRecording(false);
+    }
+  };
+
+  const getSources = async () => {
+    const inputSources = await desktopCapturer.getSources({
+      types: ["window", "screen"],
+    });
+
+    const videoOptionsMenu = Menu.buildFromTemplate(
+      inputSources.map((source) => {
+        return {
+          label: source.name,
+          click: () => selectSource(source),
+        };
+      })
+    );
+    videoOptionsMenu.popup();
+  };
+
+  const selectSource = async (source) => {
+    setSelectContent(source.name);
+
+    const constraints = {
+      audio: false,
+      video: {
+        mandatory: {
+          chromeMediaSource: "desktop",
+          chromeMediaSourceId: source.id,
+        },
+      },
+    };
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      const audioStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+      audioPreview.current = audioStream;
+
+      const options = { mimeType: "video/webm; codecs=vp9" };
+
+      let audioTracks = audioPreview.current.getAudioTracks();
+      stream.addTrack(audioTracks[0]);
+
+      mediaRecorder = new MediaRecorder(stream, options);
+      mediaRecorder.ondataavailable = handleDataAvailable;
+      mediaRecorder.onstop = handleStop;
+      setMediaRecorder(mediaRecorder);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDataAvailable = (e) => {
+    recordedChunks.push(e.data);
+  };
+
+  const handleStop = async (e) => {
+    const blob = new Blob(recordedChunks, {
+      type: "video/webm; codecs=vp9",
+    });
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const { filePath } = await dialog.showSaveDialog({
+      buttonLabel: "Save video",
+      defaultPath: `vid-${Date.now()}.webm`,
+    });
+
+    writeFile(filePath, buffer, () => {
+      console.log("Video saved successfully");
+    });
+  };
+
+  return (
+    <div className="App">
+      <main>
+        <h1>
+          ERDesktop
+          {selectContent === "Select Source" ? "" : ` - ${selectContent}`}
+        </h1>
+
+        <video id="videoPreview" autoPlay ref={videoPreview}></video>
+
+        <div className="button-container">
+          <button
+            id="startVideo"
+            onClick={startVideo}
+            style={{ backgroundColor: isRecording ? "gray" : "white" }}
+          >
+            Start
+          </button>
+          <button
+            id="stopVideo"
+            onClick={stopVideo}
+            style={{ backgroundColor: isRecording ? "white" : "gray" }}
+          >
+            Stop
+          </button>
+          <button id="selectVideo" onClick={getSources}>
+            Source: {selectContent}
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
